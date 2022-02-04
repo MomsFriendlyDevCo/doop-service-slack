9745a42cc50c5dc26682c68bafad1bb0adf38654
Mr-Yellow <mr-yellow@mr-yellow.com>
2022-02-04 10:40:20 +1000

    devops: Updated build

--- slack.gulp.js
+++ slack.gulp.js
@@ -1,5 +1,8 @@
+const debug = require('debug')('doop:slack');
+debug('Init gulp slack');
+
 const gulp = require('gulp');
 
-gulp.task.once('load:app.slack', 'load:app', ()=>
+gulp.task.once('load:app.slack', gulp.series('load:app'), ()=>
 	app.emit('load:slack')
 );
